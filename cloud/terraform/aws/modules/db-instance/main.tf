caed1ee9d85f1efd80ab623a8ec67c48a81210b5
jaredgorski <jared.gorski@liferay.com>
2026-05-12 19:57:00 -0400

    LCD-51519 Fix IAM role name_prefix length

--- cloud/terraform/aws/modules/db-instance/main.tf
+++ cloud/terraform/aws/modules/db-instance/main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role" "rds_monitoring" {
 			Version="2012-10-17"
 		})
 	force_detach_policies=true
-	name_prefix="${substr(var.identifier, 0, 38)}-rds-mon-"
+	name_prefix="${substr(var.identifier, 0, 23)}-rds-mon-"
 }
 resource "aws_iam_role_policy_attachment" "rds_monitoring" {
 	policy_arn="arn:${var.arn_partition}:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
