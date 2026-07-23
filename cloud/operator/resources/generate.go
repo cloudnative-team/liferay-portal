1341fc60272bec2a0aeb929f6be4f7515261d4e0
Gregory Amerson <gregory.amerson@liferay.com>
2026-07-23 17:25:15 -0500

    LCD-52685 copy generated crd into helm chart

--- cloud/operator/resources/generate.go
+++ cloud/operator/resources/generate.go
@@ -1,4 +1,5 @@
 //go:generate go tool controller-gen crd paths=./... output:crd:artifacts:config=config/crd/bases
+//go:generate cp config/crd/bases/licensing.liferay.com_liferayenvironments.yaml ../../helm/operator/crds/licensing.liferay.com_liferayenvironments.yaml
 //go:generate go tool controller-gen object paths=./api/...
 //go:generate go tool controller-gen rbac:roleName=manager-role paths=./... output:rbac:artifacts:config=config/rbac
 
