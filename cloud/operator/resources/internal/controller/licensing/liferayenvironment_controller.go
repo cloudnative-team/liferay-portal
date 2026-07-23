18813b6d43ec3976fba5ddea8e78d64586acdbcb
Gregory Amerson <gregory.amerson@liferay.com>
2026-07-23 11:27:27 -0500

    LCD-52685 no-op LiferayEnvironmentReconciler

---
+++ cloud/operator/resources/internal/controller/licensing/liferayenvironment_controller.go
@@ -0,0 +1,72 @@
+package licensing
+
+import (
+	"context"
+	"time"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	licensingv1alpha1 "github.com/liferay/liferay-portal/cloud/operator/api/licensing/v1alpha1"
+)
+
+type LiferayEnvironmentReconciler struct {
+	client.Client
+
+	HeartbeatInterval time.Duration
+}
+
+// +kubebuilder:rbac:groups=licensing.liferay.com,resources=liferayenvironments,verbs=get;list;watch;update;patch
+// +kubebuilder:rbac:groups=licensing.liferay.com,resources=liferayenvironments/finalizers,verbs=update
+// +kubebuilder:rbac:groups=licensing.liferay.com,resources=liferayenvironments/status,verbs=get;update;patch
+func (r *LiferayEnvironmentReconciler) Reconcile(
+	ctx context.Context,
+	req ctrl.Request,
+) (ctrl.Result, error) {
+	lenv := &licensingv1alpha1.LiferayEnvironment{}
+
+	if err := r.Get(ctx, req.NamespacedName, lenv); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	if lenv.Status.Phase == "" {
+		lenv.Status.Phase = "Pending"
+	}
+
+	meta.SetStatusCondition(
+		&lenv.Status.Conditions,
+		metav1.Condition{
+			Type:    "Ready",
+			Status:  metav1.ConditionFalse,
+			Reason:  "NotImplemented",
+			Message: "Reconcile is not implemented.",
+		},
+	)
+
+	status := r.Status()
+
+	if err := status.Update(ctx, lenv); err != nil {
+		if apierrors.IsConflict(err) {
+			return ctrl.Result{RequeueAfter: time.Second}, nil
+		}
+
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{RequeueAfter: r.HeartbeatInterval}, nil
+}
+
+func (r *LiferayEnvironmentReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(
+		mgr,
+	).For(
+		&licensingv1alpha1.LiferayEnvironment{},
+	).Named(
+		"liferayenvironment",
+	).Complete(
+		r,
+	)
+}
