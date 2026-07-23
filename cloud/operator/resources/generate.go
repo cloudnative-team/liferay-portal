d512560a0f1323cfe8d28a8c3aed3289d7585868
Gregory Amerson <gregory.amerson@liferay.com>
2026-07-23 11:46:31 -0500

    LCD-52685 add rbac generator

--- cloud/operator/resources/generate.go
+++ cloud/operator/resources/generate.go
@@ -1,4 +1,5 @@
-//go:generate go tool controller-gen object paths=./api/...
 //go:generate go tool controller-gen crd paths=./... output:crd:artifacts:config=config/crd/bases
+//go:generate go tool controller-gen object paths=./api/...
+//go:generate go tool controller-gen rbac:roleName=manager-role paths=./... output:rbac:artifacts:config=config/rbac
 
 package main
