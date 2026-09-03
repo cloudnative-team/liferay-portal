ff02269e38b31fbad79c3da26c33197320460593
Gregory Amerson <gregory.amerson@liferay.com>
2026-09-02 22:45:54 -0500

    LCD-53176 fix tfstate bucket name

--- cloud/scripts/setup_aws.sh
+++ cloud/scripts/setup_aws.sh
@@ -106,9 +106,10 @@ function _check_utils {
 }
 
 function _configure_s3_bucket {
-	local alias_name="alias/tfstate-${bucket_name}"
 	local bucket_name="${1}"
 	local region="${2}"
+
+	local alias_name="alias/tfstate-${bucket_name}"
 
 	local account_id
 
